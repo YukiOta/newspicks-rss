2121ccb3c5182b3b99e2d5f021e5b3d7f04a6ac2
YukiOta <yuki.ota@ruri.waseda.jp>
2019-03-17 19:52:26 +0900

    :zap: update feed

--- routes/feed.js
+++ routes/feed.js
@@ -48,52 +48,50 @@ Router.get("/np/original", (req, res, next) => {
       ).format("X");
       const last_updated_ts_at = module.parent.exports.get("last_update_at");
 
-      if (latest_updated_ts_at > last_updated_ts_at) {
-        // new articles
-        $(".news-card").each((i, elem) => {
-          const id = $(elem).data().id;
-          const key = $(elem).data().key;
-          const update_at = moment(String(key), "YYYYMMDDHHmmssSSS").format(
-            "X"
-          );
-          if (update_at > last_updated_ts_at) {
-            const title = $(elem)
-              .children("a")
-              .children(".title")
-              .text();
-            const content = $(elem)
-              .children(".user-comment")
-              .text();
+      // if (latest_updated_ts_at > last_updated_ts_at) {
+      // new articles
+      $(".news-card").each((i, elem) => {
+        const id = $(elem).data().id;
+        const key = $(elem).data().key;
+        const update_at = moment(String(key), "YYYYMMDDHHmmssSSS").format("X");
+        if (update_at > last_updated_ts_at) {
+          const title = $(elem)
+            .children("a")
+            .children(".title")
+            .text();
+          const content = $(elem)
+            .children(".user-comment")
+            .text();
 
-            const article = {
-              title: title,
-              id: id,
-              link: `https://newspicks.com/news/${id}`,
-              description: title,
-              content: content,
-              date: moment(String(key), "YYYYMMDDHHmmssSSS"),
-              image: `https://contents.newspicks.com/images/news/${id}`
-            };
+          const article = {
+            title: title,
+            id: id,
+            link: `https://newspicks.com/news/${id}`,
+            description: title,
+            content: content,
+            date: moment(String(key), "YYYYMMDDHHmmssSSS"),
+            image: `https://contents.newspicks.com/images/news/${id}`
+          };
 
-            feed.addItem({
-              title: article.title,
-              id: article.id,
-              link: article.link,
-              description: article.description,
-              content: article.content,
-              date: article.date,
-              image: article.image
-            });
-          }
-        });
-        module.parent.exports.set("last_update_at", latest_updated_ts_at);
-        res.set({
-          "Content-Type": "application/atom+xml"
-        });
-        res.status(200).send(feed.atom1());
-      } else {
-        res.status(304).end();
-      }
+          feed.addItem({
+            title: article.title,
+            id: article.id,
+            link: article.link,
+            description: article.description,
+            content: article.content,
+            date: article.date,
+            image: article.image
+          });
+        }
+      });
+      module.parent.exports.set("last_update_at", latest_updated_ts_at);
+      res.set({
+        "Content-Type": "application/atom+xml"
+      });
+      res.status(200).send(feed.atom1());
+      // } else {
+      //   res.status(304).end();
+      // }
     } catch (error) {
       console.log("error: ", error);
       next();
